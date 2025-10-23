0a3549da3e48ecfd313bc501e0280891a3d32b6b
mgl0067 <mgl0067@alu.medac.es>
2025-10-23 16:01:17 +0200

    23/10/2025

--- Tarea evaluable parte 1 JavaScript/script.js
+++ Tarea evaluable parte 1 JavaScript/script.js
@@ -1,22 +1,3 @@
-
-/*function agregarComentario () {
-        var texto = document.getElementById("comentario").value; //La variable texto es = al valor que se obtiene de "comentario"
-
-        var parrafo = document.createElement("p"); //La variable nuevo es = a la creación de un p
-        parrafo.style.color = "red";
-
-        var texto = document.createTextNode(texto); //A partir de la variable texto creamos un nodo de texto con el valor introducido en "comentario"
-
-        parrafo.appendChild(texto); //Metemos el texto dentro del parrafo
-        listaComentarios.appendChild(texto) //Metemos el texto dentro del div
-        listaComentarios.style.color = "blue";
-        listaComentarios.style.backgroundColor = "red";
-
-
-      } */
-
-
-
 function agregarComentario () {
 var texto = document.getElementById("comentario").value; //La variable texto es = al valor que se obtiene de "comentario"
 
@@ -27,8 +8,8 @@ var nodo = document.createTextNode(texto); //A partir de la variable texto cream
 parrafo.appendChild(nodo); 
 listaComentarios.appendChild(parrafo);
 
-parrafo.style.color = "red";
-listaComentarios.style.backgroundColor = "grey";
+parrafo.style.color = "blue";
+listaComentarios.style.backgroundColor = "#ABABAB";
 
-
+document.getElementById("comentario").value = "";
 } 
